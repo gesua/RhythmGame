d5171d0d44047c8647e56f7dec894dba0183cd98
yudaheed <snrnsrk951@naver.com>
2018-06-08 16:01:37 +0900

    노래 추가

--- app/src/main/java/com/example/kitri/rhythmgame/BeatThread.java
+++ app/src/main/java/com/example/kitri/rhythmgame/BeatThread.java
@@ -9,10 +9,45 @@ public class BeatThread extends Thread {
     private PlayActivity context;
 
     private Beat[] beats = {
-            new Beat(1000, "1"),
-            new Beat(2000, "2"),
-            new Beat(3000, "3"),
-            new Beat(4000, "4"),
+            new Beat(600, "1"),
+            new Beat(700, "3"),
+            new Beat(800, "4"),
+            new Beat(1200, "1"),
+            new Beat(1300, "3"),
+            new Beat(1400, "4"),
+            new Beat(1800, "1"),
+            new Beat(1900, "3"),
+            new Beat(2000, "4"),
+
+            new Beat(2400, "1"),
+            new Beat(2500, "4"),
+            new Beat(2600, "3"),
+            new Beat(3600, "2"),
+            new Beat(3700, "4"),
+            new Beat(3800, "3"),
+            new Beat(4200, "3"),
+            new Beat(4300, "1"),
+            new Beat(4400, "2"),
+
+            new Beat(4800, "3"),
+            new Beat(4900, "1"),
+            new Beat(5000, "2"),
+            new Beat(5400, "4"),
+            new Beat(5500, "2"),
+            new Beat(5600, "1"),
+            new Beat(6000, "3"),
+            new Beat(6100, "1"),
+            new Beat(6200, "2"),
+
+            new Beat(6600, "3"),
+            new Beat(6700, "1"),
+            new Beat(6800, "2"),
+            new Beat(7200, "1"),
+            new Beat(7300, "4"),
+            new Beat(7400, "3"),
+            new Beat(7800, "4"),
+            new Beat(7900, "2"),
+            new Beat(8000, "1")
     };
 
     public BeatThread(PlayActivity context) {
