9379489006c0b9fea26174f165133204b735705b
gesua <gesua@hanmail.net>
2018-06-12 10:00:35 +0900

    인게임 스코어 이미지 추가

--- app/src/main/java/com/example/kitri/rhythmgame/NumManager.java
+++ app/src/main/java/com/example/kitri/rhythmgame/NumManager.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.ImageView;
 
-public class Combo {
+public class NumManager {
     private ImageView[] iv_combo = new ImageView[3];
+    private ImageView[] iv_score = new ImageView[4];
     private int[] numImage = new int[10];
 
-    public Combo(Activity context) {
+    public NumManager(Activity context) {
         iv_combo[0] = context.findViewById(R.id.tv_combo_1);
         iv_combo[1] = context.findViewById(R.id.tv_combo_2);
         iv_combo[2] = context.findViewById(R.id.tv_combo_3);
+
+        iv_score[0] = context.findViewById(R.id.iv_score_1);
+        iv_score[1] = context.findViewById(R.id.iv_score_2);
+        iv_score[2] = context.findViewById(R.id.iv_score_3);
+        iv_score[3] = context.findViewById(R.id.iv_score_4);
 
         numImage[0] = R.drawable.num_0;
         numImage[1] = R.drawable.num_1;
@@ -40,8 +46,37 @@ public class Combo {
             iv_combo[i].setVisibility(View.VISIBLE);
         }
 
-        for(int j=i;j<iv_combo.length;j++){
+        for (int j = i; j < iv_combo.length; j++) {
             iv_combo[j].setVisibility(View.GONE);
+        }
+    }
+
+    public void setScore(int score) {
+        if (score == 0) {
+            iv_score[0].setImageResource(numImage[0]);
+
+            for (int i = 1; i < iv_score.length; i++) {
+                iv_score[i].setVisibility(View.GONE);
+            }
+            return;
+        }
+
+        int temp, cnt = 0;
+        while (score > 0) {
+            temp = score;
+            temp %= 10;
+            iv_score[cnt++].setImageResource(numImage[temp]);
+
+            score /= 10;
+        }
+
+        int i;
+        for (i = 0; i < cnt; i++) {
+            iv_score[i].setVisibility(View.VISIBLE);
+        }
+
+        for (int j = i; j < iv_score.length; j++) {
+            iv_score[j].setVisibility(View.GONE);
         }
     }
 }
